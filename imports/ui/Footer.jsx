61c1844b06c2b310eaec8803ed3b4f8d89966f10
Javier Carceller <elxavicio@gmail.com>
2019-02-20 10:44:43 +0800

    Makes footer position fixed and improves its content

--- imports/ui/Footer.jsx
+++ imports/ui/Footer.jsx
@@ -14,62 +14,32 @@ const Footer = () => (
   <Segment
     inverted
     vertical
-    style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
+    style={{ bottom: "0", position: "absolute", width: "100%" }}
   >
     <Container textAlign="center">
       <Grid divided inverted stackable>
-        <Grid.Column width={3}>
-          <Header inverted as="h4" content="Group 1" />
-          <List link inverted>
-            <List.Item as="a">Link One</List.Item>
-            <List.Item as="a">Link Two</List.Item>
-            <List.Item as="a">Link Three</List.Item>
-            <List.Item as="a">Link Four</List.Item>
-          </List>
+        <Grid.Column width={13}>
+          <Header
+            inverted
+            as="h4"
+            content="Made with love by Javier Carceller"
+          />
+          <p>Get in touch and let me know how to improve this!</p>
         </Grid.Column>
         <Grid.Column width={3}>
-          <Header inverted as="h4" content="Group 2" />
           <List link inverted>
-            <List.Item as="a">Link One</List.Item>
-            <List.Item as="a">Link Two</List.Item>
-            <List.Item as="a">Link Three</List.Item>
-            <List.Item as="a">Link Four</List.Item>
+            <List.Item as="a">
+              <a href="https://github.com/elxavicio">GitHub</a>
+            </List.Item>
+            <List.Item as="a">
+              <a href="https://www.linkedin.com/in/javiercarceller/">
+                LinkedIn
+              </a>
+            </List.Item>
+            <a href="https://javiercarceller.com/">Portfolio</a>
           </List>
-        </Grid.Column>
-        <Grid.Column width={3}>
-          <Header inverted as="h4" content="Group 3" />
-          <List link inverted>
-            <List.Item as="a">Link One</List.Item>
-            <List.Item as="a">Link Two</List.Item>
-            <List.Item as="a">Link Three</List.Item>
-            <List.Item as="a">Link Four</List.Item>
-          </List>
-        </Grid.Column>
-        <Grid.Column width={7}>
-          <Header inverted as="h4" content="Footer Header" />
-          <p>
-            Extra space for a call to action inside the footer that could help
-            re-engage users.
-          </p>
         </Grid.Column>
       </Grid>
-
-      <Divider inverted section />
-      <Image centered size="mini" src="/logo.png" />
-      <List horizontal inverted divided link size="small">
-        <List.Item as="a" href="#">
-          Site Map
-        </List.Item>
-        <List.Item as="a" href="#">
-          Contact Us
-        </List.Item>
-        <List.Item as="a" href="#">
-          Terms and Conditions
-        </List.Item>
-        <List.Item as="a" href="#">
-          Privacy Policy
-        </List.Item>
-      </List>
     </Container>
   </Segment>
 );
